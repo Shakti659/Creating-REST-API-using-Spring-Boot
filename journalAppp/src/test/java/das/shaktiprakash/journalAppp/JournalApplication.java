8bdd16a6c0c76fd90980ee467a4e5eb2d904247e
Shakti659 <88531659+Shakti659@users.noreply.github.com>
2025-07-26 01:13:44 +0530

    initial commit

---
+++ journalAppp/src/test/java/das/shaktiprakash/journalAppp/JournalApplication.java
@@ -0,0 +1,13 @@
+package das.shaktiprakash.journalAppp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootApplication
+public class JournalApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JournalApplication.class, args);
+	}
+}
